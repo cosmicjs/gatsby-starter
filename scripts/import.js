1b93a755917c04d6f5bc29965e58720bcd182cd3
Tony Spiro <tspiro@tonyspiro.com>
2020-03-24 16:04:00 -0500

    FIX: read key

--- scripts/import.js
+++ scripts/import.js
@@ -3,17 +3,22 @@ const api = Cosmic()
 const COSMIC_BUCKET = process.env.COSMIC_BUCKET || 'node-starter'
 const bucket = api.bucket({
 	slug: COSMIC_BUCKET,
+	read_key: process.env.COSMIC_READ_KEY,
 	write_key: process.env.COSMIC_WRITE_KEY
 })
 const default_objects = require('./objects')
 const default_object_type = require('./object-type')
 const importObjects = async () => {
-	const res = await bucket.getObjects()
-	if(res.status === 'empty') {
-		bucket.addObjectType(default_object_type)
-		default_objects.forEach(object => {
-			bucket.addObject(object)
-		})
+	try {
+		const res = await bucket.getObjects()
+		if(res.status === 'empty') {
+			bucket.addObjectType(default_object_type)
+			default_objects.forEach(object => {
+				bucket.addObject(object)
+			})
+		}
+	} catch (e) {
+		console.log(e)
 	}
 }
 importObjects()
